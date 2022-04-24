d3e4632c7a105ca47b9d8f0fe4ec219eb15faa7f
mnaofficial <mna.official92@gmail.com>
2022-04-24 19:51:42 +0600

    add product to cart modify

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,62 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import useProducts from '../../hooks/useProducts';
-import { addToDb, getStoredCart } from '../../utilities/fakedb';
+import useCart from '../../hooks/useCart';
+import { addToDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
-    const [products, setProducts] = useProducts();
-    const [cart, setCart] = useState([]);
+    
+    const [cart, setCart] = useCart();
     const [pageCount, setPageCount] = useState(0);
     const [page, setPage] = useState(0);
-    const [size, setSize] = useState(10)
+    const [size, setSize] = useState(10);
+    const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        const storedCart = getStoredCart();
-        const savedCart = [];
-        for (const id in storedCart) {
-            const addedProduct = products.find(product => product._id === id);
-            if (addedProduct) {
-                const quantity = storedCart[id];
-                addedProduct.quantity = quantity;
-                savedCart.push(addedProduct);
-            }
-        }
-        setCart(savedCart);
-    }, [products]);
+    useEffect( () =>{
+        fetch(`http://localhost:5000/product?page=${page}&size=${size}`)
+        .then(res => res.json())
+        .then(data => setProducts(data));
+    }, [page, size]);
 
-    useEffect(() => {
-        fetch(`http://localhost:5000/product?page=${page}&size${size}`)
-            .then(res => res.json())
-            .then(data => setProducts(data));
-    }, []);
-
-    useEffect(() => {
+    useEffect( () =>{
         fetch('http://localhost:5000/productCount')
-            .then(res => res.json())
-            .then(data => {
-                const count = data.count;
-                const pages = Math.ceil(count / 10)
-                setPageCount(pages);
-            })
+        .then(res => res.json())
+        .then(data =>{
+            const count = data.count;
+            const pages = Math.ceil(count/10);
+            setPageCount(pages);
+        })
     }, [])
 
-    const handleAddToCart = (selectedProduct) => {
+    
+
+    const handleAddToCart = (selectedProduct) =>{
         console.log(selectedProduct);
         let newCart = [];
         const exists = cart.find(product => product._id === selectedProduct._id);
-        if (!exists) {
+        if(!exists){
             selectedProduct.quantity = 1;
             newCart = [...cart, selectedProduct];
         }
-        else {
+        else{
             const rest = cart.filter(product => product._id !== selectedProduct._id);
             exists.quantity = exists.quantity + 1;
             newCart = [...rest, exists];
         }
-
+        
         setCart(newCart);
         addToDb(selectedProduct._id);
     }
@@ -65,23 +54,24 @@ const Shop = () => {
         <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product => <Product
+                    products.map(product=><Product 
                         key={product._id}
                         product={product}
                         handleAddToCart={handleAddToCart}
-                    ></Product>)
+                        ></Product>)
                 }
                 <div className='pagination'>
                     {
                         [...Array(pageCount).keys()]
-                            .map(number => <button
-                                className={page === number ? 'selected' : ''}
-                                onClick={() => setPage(number)}
-                            >{number + 1}</button>)
+                        .map(number => <button
+                            className={page=== number ? 'selected': ''}
+                            onClick={() => setPage(number)}
+                        >{number + 1}</button>)
                     }
+                    
                     <select onChange={e => setSize(e.target.value)}>
                         <option value="5">5</option>
-                        <option value="10">10</option>
+                        <option value="10" selected>10</option>
                         <option value="15">15</option>
                         <option value="20">20</option>
                     </select>
